5756ba0a1eae83f84f5916c1112944244d208de4
ye-russell <bekberata@gmail.com>
2021-03-11 16:44:32 +0600

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let stringed = String(n);
+  let reversed = "";
+  for (i = stringed.length; i > 0; i--) {
+    reversed += stringed[i-1]
+  }
+  return parseInt(reversed);
 }
